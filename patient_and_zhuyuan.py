bf6c27ee29b1890a0762027e0328046a9eea2bd6
Kaixin Liu <254227712@qq.com>
2020-07-03 14:01:56 +0800

    Add files via upload
    更改了输入和输出

--- patient_and_zhuyuan.py
+++ patient_and_zhuyuan.py
@@ -1,5 +1,6 @@
 # *_*coding:utf-8 *_*
 
+from itertools import permutations
 from apriori import apriori
 import rdflib
 import os, re, time, sys
@@ -33,7 +34,10 @@ def match_path(g,path,path1,dict_forward):
         for sub in g.subjects():
             if re.search(path[0], sub) is None:
                 continue
-            match_result[sub] = [sub]
+            if match_result.get(sub, -1) == -1:
+                match_result[sub] = {sub}
+            else:
+                match_result[sub].add(sub)
     else:
         if int(path1[0]) == 1:
             for sub in g.subjects():
@@ -93,6 +97,75 @@ def match_path(g,path,path1,dict_forward):
             list_tmp = list(set(list_tmp))
             record.append(list_tmp)
     return record
+def match_path2(g,path,path1,dict_forward):
+    match_result = {}
+    if len(path) == 1:
+        for sub in g.subjects():
+            if sub.find(path[0]) < 0:
+                continue
+            if match_result.get(sub, -1) == -1:
+                match_result[sub] = {sub}
+            else:
+                match_result[sub].add(sub)
+    else:
+        if int(path1[0]) == 1:
+            for sub in g.subjects():
+                if sub.find(path[0]) < 0:
+                    continue
+                for pre, obj in g.predicate_objects(sub):
+                    if obj.find(path[1]) < 0:
+                        continue
+                    if match_result.get(sub, -1) == -1:
+                        match_result[sub] = {obj}
+                    else:
+                        match_result[sub].add(obj)
+        else:
+            for obj in g.objects():
+                if obj.find(path[0]) < 0:
+                    continue
+                for sub, pre in g.subject_predicates(obj):
+                    if sub.find(path[1]) < 0:
+                        continue
+                    if match_result.get(obj, -1) == -1:
+                        match_result[obj] = {sub}
+                    else:
+                        match_result[obj].add(sub)
+        for idx, val in enumerate(path1[1:], 1):
+            if int(val) == 1:
+                for key, values in match_result.items():
+                    set_tmp = set()
+                    for value in values:
+                        for pre, obj in g.predicate_objects(value):
+                            if obj.find(path[idx + 1]) < 0:
+                                continue
+                            set_tmp.add(obj)
+                    match_result[key] = set_tmp
+            else:
+                for key, values in match_result.items():
+                    set_tmp = set()
+                    for value in values:
+                        for sub, pre in g.subject_predicates(value):
+                            if sub.find(path[idx + 1]) < 0:
+                                continue
+                            set_tmp.add(sub)
+                    match_result[key] = set_tmp
+    record = []
+    for key, values in match_result.items():
+        for value in values:
+            list_tmp = []
+            for pre, obj in g.predicate_objects(key):
+                list_tmp.append(dict_forward[str(obj)])
+            for sub, pre in g.subject_predicates(key):
+                list_tmp.append(dict_forward[str(sub)])
+            if key == value:
+                continue
+            for pre, obj in g.predicate_objects(value):
+                list_tmp.append(dict_forward[str(obj)])
+            for sub, pre in g.subject_predicates(value):
+                list_tmp.append(dict_forward[str(sub)])
+            list_tmp = list(set(list_tmp))
+            record.append(list_tmp)
+    return record
 
 def match_sparql(g,sparql_query,dict_forward):
     qres = g.query(sparql_query)
@@ -122,14 +195,14 @@ def write_file(graphfile, record,query_node,dict_reverse):
     itemfile_write = open(itemfile, 'a', encoding='utf-8')
     rulesfile_write = open(rulesfile, 'a', encoding='utf-8')
 
-    print(len(record))
+    #print(len(record))
     itemsets, rules = apriori(record,query_node=query_node, min_support=0.5, min_confidence=0.9)
     for (key, value) in itemsets.items():
         itemset = []
         for item_tuple in value.keys():
             for item in item_tuple:
                 itemset.append(dict_reverse[item])
-        print(itemset)
+        #print(itemset)
         itemfile_write.write(str(itemset))
         itemfile_write.write("\n")
     rule_dict = {}
@@ -145,13 +218,11 @@ def write_file(graphfile, record,query_node,dict_reverse):
                 rule_dict[dict_reverse[item]] = [str(lhs_itemset) + "->" + str(rhs_itemset)+"\tsupport="+str(rule.support)+"\tconfidence="+str(rule.confidence)+'\tlift='+str(rule.lift)]
             else:
                 rule_dict[dict_reverse[item]].append(str(lhs_itemset) + "->" + str(rhs_itemset)+"\tsupport="+str(rule.support)+"\tconfidence="+str(rule.confidence)+'\tlift='+str(rule.lift))
-        print(lhs_itemset)
-        print(rhs_itemset)
+
         rulesfile_write.write(str(lhs_itemset))
         rulesfile_write.write("->")
         rulesfile_write.write(str(rhs_itemset))
         rulesfile_write.write("\n")
-    print("graph has %s statements." % len(g))
     with open(rulesdict_file, 'w', encoding='utf8') as outfile:
         for key, values in rule_dict.items():
             outfile.write(key + ":[")
@@ -176,6 +247,58 @@ def get_query_node_result(query_node,rule_dict,dict_reverse):
     with open("query_result.txt",'w',encoding='utf8') as outfile:
         for rule in rule_set:
             outfile.write(rule+'\n')
+    for i in range(len(query_node)):
+        query_node[i]=dict_reverse[query_node[i]]
+
+    for p in permutations(query_node):
+        for i in range(1,len(query_node)):
+            rule_str='[\''
+            for j in range(i):
+                rule_str+=p[j]+'\', \''
+            rule_str=rule_str[:-3]+']->[\''
+            for j in range(i,len(p)):
+                rule_str+=p[j]+'\', \''
+            rule_str=rule_str[:-3]+']'
+            result_dict={"rule":rule_str}
+            for rule in rule_set:
+                if rule.find(rule_str)>=0:
+                    re_group=re.search('support=(.*?)confidence=(.*?)lift=(.*)', rule)
+                    result_dict["support"]=re_group[1]
+                    result_dict["confidence"]=re_group[2]
+                    result_dict["lift"]=re_group[3]
+                    print(result_dict)
+
+
+
+
+def main_algorithm(args):
+    graphfile = args.graph_file
+    g = rdflib.Graph()
+    result = g.parse(graphfile, format="nt")
+    dict_forward,dict_reverse=get_index(g)
+    query_node=args.query_node.strip('\n').split(',')
+    for i in range(len(query_node)):
+        query_node[i]='http://www.tsinghua-west.com/Guxi/'+query_node[i]
+
+    if args.query_type == 'path':
+        path_node_list=args.query_file.strip('\n').split(',')
+        path=path_node_list[:int((len(path_node_list)+1)/2)]
+        path1=path_node_list[int((len(path_node_list)+1)/2):]
+        #print(path,path1)
+        match_result = match_path2(g, path, path1, dict_forward)
+        for i in range(len(query_node)):
+            query_node[i]=dict_forward[query_node[i]]
+
+        rule_dict=write_file(g,match_result,query_node,dict_reverse)
+        get_query_node_result(query_node,rule_dict,dict_reverse)
+    else:
+        sparql_query=args.query_file
+        sparql_query=sparql_query.replace('_',' ')
+        match_result = match_sparql(g, sparql_query, dict_forward)
+        for i in range(len(query_node)):
+            query_node[i]=dict_forward[query_node[i]]
+        rule_dict = write_file(g, match_result, query_node, dict_reverse)
+        get_query_node_result(query_node, rule_dict, dict_reverse)
 
 if __name__ == '__main__':
     start_time=time.time()
@@ -183,72 +306,10 @@ if __name__ == '__main__':
     basic = parser.add_argument('-g', '--graph_file',dest='graph_file', type=str, required=True, help='input graph file')
     basic = parser.add_argument('-t', '--query_type',dest='query_type', type=str, required=True, help='query type')
     basic = parser.add_argument('-q', '--query_file', dest='query_file', type=str, required=True, help='query file')
-    if len(sys.argv[1:])!=6:
+    basic = parser.add_argument('-x', '--query_node', dest='query_node', type=str, required=True, help='query node')
+    if len(sys.argv[1:])!=8:
         parser.print_help()        #print usage
         parser.exit()
     args = parser.parse_args()
-    graphfile = args.graph_file
-    g = rdflib.Graph()
-    result = g.parse(graphfile, format="nt")
-    end_time=time.time()
-    print('time cost : %.5f sec' %(end_time-start_time))
-    dict_forward,dict_reverse=get_index(g)
-    end_time=time.time()
-    print('time cost : %.5f sec' %(end_time-start_time))
-    if args.query_type == 'path':
-        with open(args.query_file,'r',encoding='utf-8') as query:
-            lines=query.readlines()
-            query_node=lines[0].strip('\n').split(',')
-            path = lines[1].strip('\n').split(',')
-            path1 = lines[2].strip('\n').split(',')
-        match_result=match_path(g,path,path1,dict_forward)
-        end_time=time.time()
-        print('time cost : %.5f sec' %(end_time-start_time))
 
-        for i in range(len(query_node)):
-            query_node[i]=dict_forward[query_node[i]]
-
-        rule_dict=write_file(g,match_result,query_node,dict_reverse)
-        get_query_node_result(query_node,rule_dict,dict_reverse)
-
-        '''
-        ap = apriori_me.Apriori(match_result,nodeSet=query_node, minSupport=0.5, minConf=0.9)
-        print(ap.resultConf)
-        print(ap.resultSupport)
-        
-        result_rule={}
-        for item in ap.resultConf:
-            item_list=apriori_me.str2itemsets[item]
-            for item
-            result_rule.setdefault(dict_reverse[item])
-            new_list=[]
-            for iitem in ap.resultConf[item]:
-                new_list.append(dict_reverse[iitem])
-            result_rule[dict_reverse[item]].setdefault(new_list,
-                                                     ap.resultConf[])
-        
-        '''
-        end_time=time.time()
-        print('time cost : %.5f sec' %(end_time-start_time))
-    else:
-        file_object = open(args.query_file,'r',encoding='utf-8')
-        try:
-            query = file_object.readlines()
-            query_node=query[0].strip('\n').split(',')
-            sparql_query=''.join(query[1:])
-        finally:
-            file_object.close()
-        match_result=match_sparql(g,sparql_query,dict_forward)
-        end_time=time.time()
-        print('time cost : %.5f sec' %(end_time-start_time))
-        for i in range(len(query_node)):
-            query_node[i]=dict_forward[query_node[i]]
-        '''
-        ap = apriori_me.Apriori(match_result,nodeSet=query_node, minSupport=0.5, minConf=0.9)
-        print(ap.resultConf)
-        print(ap.resultSupport)
-        '''
-        rule_dict=write_file(g,match_result,query_node,dict_reverse)
-        get_query_node_result(query_node,rule_dict,dict_reverse)
-        end_time=time.time()
-        print('time cost : %.5f sec' %(end_time-start_time))
+    main_algorithm(args)
